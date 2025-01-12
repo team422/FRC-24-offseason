301330bffc66ce10b661af115403f84a151df7cb
keckothedragon <flappystar05@gmail.com>
2025-01-12 12:42:14 -0500

    Automated commit on 01/12/2025 12:42:13

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -109,7 +109,7 @@ public final class Constants {
         new Transform3d[] {
           // front right (shooter)
           new Transform3d(
-              new Translation3d(0.270000, -0.137414, 0.197256),
+              new Translation3d(0.270000, -0.137414, 0.065),
               new Rotation3d(-0.030000, -0.600000, -0.150000)),
 
           // back right (intake)
@@ -130,7 +130,7 @@ public final class Constants {
 
           // front left (shooter)
           new Transform3d(
-              new Translation3d(0.270000, 0.127414, 0.197256),
+              new Translation3d(0.270000, 0.127414, 0.065),
               new Rotation3d(-0.030000, -0.600000, 0.150000)),
         };
 

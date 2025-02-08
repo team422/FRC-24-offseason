16af4acd70b96cfec00ccb57c64275b5ea1a3eca
keckothedragon <flappystar05@gmail.com>
2025-02-08 12:48:26 -0500

    Automated commit on 02/08/2025 12:48:26

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -112,7 +112,7 @@ public final class Constants {
 
     // tolerances for using the vision rotation, temp values
     public static final double kRotationErrorThreshold = 0.1;
-    public static final double kRotationDistanceThreshold = Units.inchesToMeters(8);
+    public static final double kRotationDistanceThreshold = Units.inchesToMeters(36);
     public static final double kRotationSpeedThreshold = 0.2; // m/s
 
     // transform from center of robot to camera
